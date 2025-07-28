119cbc2f3be98d2b304f4600192b593759289f69
John <duanjohn99@gmail.com>
2025-07-28 18:12:44 -0400

    stuff

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,13 +4,11 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gorilla/mux"
-	"github.com/thejhndwn/sanbar/backend/internal/handler/home"
 	"github/com/thejhndwn/sanbar/backend/internal/handler/auth"
 )
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", home.HomeHandler).Methods("GET")
 	api := r.PathPrefix("/api").Subrouter()
 	api.HandleFunc("/register", auth.Register).Methods("POST")
 	api.HandleFunc("/login", auth.Login).Methods("POST")
